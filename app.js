119b2359b29ec23fff73f295988d952d105b7ecd
Iamayomi <ayomidesherif2019@gmail.com>
2024-07-02 03:57:30 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+
+app.set('trust proxy', true);
+
+app.use(express.json());
+
+app.get('/tracker/api/hello',  async function (req, res) {
+	try {
+       const ip = await req.ip;
+       const getLocation = await axios.get(`http://ip-api.com/json/${ip}`);
+
+       const { status, lat, lon, city } = getLocation.data;
+
+       if(status !== 'success'){
+       	  return res.status(400).json({ error: 'could not fetch location data'});
+       };
+
+       const getWeather = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_KEY}&q=${lat},${lon}`);
+
+       const clientData = { };
+
+       clientData.client_ip = ip;
+       clientData.location = city;
+       clientData.greeting = `Hello, ${req.query.visitor_name}, the temperature is ${getWeather.data.current.temp_c} degrees Calcius in ${city}`;
+
+       res.status(200).json({
+       	  status: "success",
+       	  data: clientData
+       })
+
+	} catch(err){
+		res.status(500).send(err.message)
+	}
+});
+
+module.exports = app;
