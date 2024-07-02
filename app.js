82e5f4e46063f4b508ae2c3cac4a9da590ff35d3
Amodu Ayomide <77242644+Iamayomi@users.noreply.github.com>
2024-07-02 15:44:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ app.set('trust proxy', true);
 
 app.use(express.json());
 
-app.get('/tracker/api/hello?visitor_name=Mark',  async function (req, res) {
+app.get('/tracker/api/hello',  async function (req, res) {
 	try {
-       const ip = await req.ip;
+       req.query.visitor_name = 'Ayomide';
+const getIp = await axios.get(`https://api.ipify.org?format=json`);
+       const ip = getIp.data.ip;
+
        const getLocation = await axios.get(`http://ip-api.com/json/${ip}`);
 
        const { status, lat, lon, city } = getLocation.data;
